46a55653088d0f672ce66f9d9049dc224cd5b535
svs <svs@svs.io>
2012-06-24 15:36:01 +0530

    trip creation and updating working
    Unauthenticated visitors can now enter their trip and email details
    and create a trip
    Capybara specs passing

--- spec/requests/index_spec.rb
+++ spec/requests/index_spec.rb
@@ -5,14 +5,17 @@ feature "the first trip" do
     @bus_stop1 = FactoryGirl.create(:bus_stop)
     @bus_stop2 = FactoryGirl.create(:bus_stop)
   end
-  
+
   scenario "creating an intended trip without having a user account" do
-    within "#new_trip" do
-      fill_in 'from', :with => @bus_stop1.name
-      fill_in 'to', :with => @bus_stop2.name
-      select 'weekdays', :from => '#on'
+    visit "/"
+    within "#new_intended_trip" do
+      select @bus_stop1.name, :from => 'intended_trip_from_stop_id'
+      select @bus_stop2.name, :from => 'intended_trip_to_stop_id'
+      choose 'intended_trip_on_weekdays'
+      fill_in 'intended_trip_user_email', :with => 'svs@svs.io'
     end
-    click_button('Create My Trip')
+    click_button('Create Intended trip')
+    page.should have_content("success")
   end
 
 end
