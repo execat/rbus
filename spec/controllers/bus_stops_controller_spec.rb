575749d498a9ff2dcddcf4767ac8321ff3052378
svs <svs@svs.io>
2012-06-24 15:35:11 +0530

    added a spec to bus_stops_controller_spec

--- spec/controllers/bus_stops_controller_spec.rb
+++ spec/controllers/bus_stops_controller_spec.rb
@@ -2,5 +2,11 @@ require 'spec_helper'
 
 
 describe BusStopsController do
-
+  describe "index" do
+    it "should return a list of bus stops" do
+      get :index
+      assigns(:bus_stops).should == BusStop.all
+    end
+  end
+    
 end
