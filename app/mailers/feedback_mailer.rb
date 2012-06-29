b7c58554b549f91f220a69a944e062ea864d591e
svs <svs@svs.io>
2012-06-30 01:54:16 +0530

    added csrf_meta_tag and from in feedback mailer

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,9 +1,8 @@
 class FeedbackMailer < ActionMailer::Base
   default to: "svs@svs.io"
-
   def feedback(email_address, feedback)
     @email_address = email_address
     @body = feedback
-    mail(:subject => "[rBus] Feedback from #{email_address}")
+    mail(:subject => "[rBus] Feedback from #{email_address}", :from => "svs@rbus.in")
   end
 end
