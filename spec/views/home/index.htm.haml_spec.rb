b5e59105c44800d96d48d0c196034d595fb96597
svs <svs@svs.io>
2012-07-01 00:21:19 +0530

    tweak index page

--- spec/views/home/index.htm.haml_spec.rb
+++ spec/views/home/index.htm.haml_spec.rb
@@ -17,9 +17,7 @@ describe "home/index" do
       assert_select "form", :action => intended_trips_path(@intended_trip), :method => "post" do
         assert_select "#intended_trip_from_stop_id"
         assert_select "#intended_trip_to_stop_id"
-        assert_select "#intended_trip_on_weekdays"
-        assert_select "#intended_trip_on_weekdays_and_saturday"
-        assert_select "#intended_trip_on_all_days"
+        assert_select "#intended_trip_on"
         assert_select "#intended_trip_user_email"
       end
     end
