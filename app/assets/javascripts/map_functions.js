ec4e652b07c6c6f0b488311207d9758c32d33619
svs <svs@svs.io>
2012-07-13 01:01:22 +0530

    can now add bus stops using the map

--- app/assets/javascripts/map_functions.js
+++ app/assets/javascripts/map_functions.js
@@ -1,24 +1,35 @@
 // requests JSON information for all the bus stops in the given viewport from the server
 // viewport is a google.maps.Bounds() object
-function loadBusStops(viewport) {
+function loadBusStops(viewport, callback) {
   y2 = viewport.getNorthEast().lat();
   x2 = viewport.getNorthEast().lng();
   y1 = viewport.getSouthWest().lat();
   x1 = viewport.getSouthWest().lng();
-  $.get('/bus_stops.json?x1=' + x1 + '&y1=' + y1 + '&x2=' + x2 + '&y2=' + y2, function(data) {
-    $.each(data, function() {
-      marker = getMarker(parseFloat(this.lat), parseFloat(this.lng), '/images/mm_20_green.png', this.name);
-      marker.setMap(map);
-    });
+  $.get('/bus_stops.json?x1=' + x1 + '&y1=' + y1 + '&x2=' + x2 + '&y2=' + y2, markBusStops);
+}
 
+function markBusStops(data)
+{
+  $.each(data, function() {
+    setBusStopMarker(this);
+    console.log(this);
   });
 }
 
 
+function setBusStopMarker(data) {
+  marker = getMarker(parseFloat(data.lat), parseFloat(data.lng), '/images/mm_20_green.png', false);
+  infoWindow = addInfoWindow(marker, "<div id='" + data.id + "' class='bs'><div>" + data.name + "</div></div>");
+  google.maps.event.addListener(infoWindow,'domready', function() {
+    addFromToLinks($('#' + data.id), data.id, data.name);
+  });
+}
+
 function getMarker(lat, lng, icon, infoWindowText) {
   m = new google.maps.Marker({
     position: new google.maps.LatLng(lat, lng),
-    icon: icon
+    icon: icon,
+    map: map
   });
   if (infoWindowText) {
     addInfoWindow(m, infoWindowText);
@@ -26,14 +37,45 @@ function getMarker(lat, lng, icon, infoWindowText) {
   return m;
 }
 
+function fromHere(id, name) {
+  $('#from').attr('value', name);
+  $('#intended_trip_from_stop_id').attr('value',id);
+}
+
+function toHere(id, name) {
+  $('#to').attr('value', name);
+  $('#intended_trip_to_stop_id').attr('value',id);
+}
+
+
+function addFromToLinks(element, id, name) {
+  fromLink = $('<a href="#">from here</a>');
+  fromLink.click(function() {
+		   fromHere(id,name);
+		   infoWindowClose();
+		 });
+  fromLink.addClass('green');
+  element.append(fromLink);
+  element.append("&nbsp;").show();
+  toLink = $('<a href="#">to here</a>');
+  toLink.click(function() {
+		   toHere(id,name);
+		   infoWindowClose();
+		 });
+  toLink.addClass('red');
+  element.append(toLink);
+
+
+}
+
 function addInfoWindow(marker, message) {
   var info = message;
   var infoWindow = new google.maps.InfoWindow({
     content: message
   });
-
   google.maps.event.addListener(marker, 'click', function () {
     infoWindow.close();
     infoWindow.open(map, marker);
   });
+  return infoWindow;
 }
