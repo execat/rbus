995e0b4e2a3357513d5b59911a1eb8cb48276f11
svs <svs@svs.io>
2012-08-23 00:37:20 +0530

    fixed sequel initializer to deal with heroku

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,2 +1,6 @@
-#ci = YAML::load_file(File.join(Rails.root,"config","database.yml"))[Rails.env]
-# DB = Sequel.connect("postgres://#{ci["username"]}:#{ci["password"]}@#{ci["hostname"] || "localhost"}/#{ci["database"]}") # Uses the postgres adapter
+if ENV["DATABASE_URL"]
+  DB = Sequel.connect(ENV["DATABASE_URL"])
+else
+  ci = YAML::load_file(File.join(Rails.root,"config","database.yml"))[Rails.env]
+  DB = Sequel.connect("postgres://#{ci["username"]}:#{ci["password"]}@#{ci["hostname"] || "localhost"}/#{ci["database"]}") # Uses the postgres adapter
+end
