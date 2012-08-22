2ea6a706a5de116370a2c4b568c8e2f37305ce26
svs <svs@svs.io>
2012-08-23 00:25:45 +0530

    heroku borking on config/sequel.rb

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,2 +1,2 @@
-ci = YAML::load_file(File.join(Rails.root,"config","database.yml"))[Rails.env]
-DB = Sequel.connect("postgres://#{ci["username"]}:#{ci["password"]}@#{ci["hostname"] || "localhost"}/#{ci["database"]}") # Uses the postgres adapter
+#ci = YAML::load_file(File.join(Rails.root,"config","database.yml"))[Rails.env]
+# DB = Sequel.connect("postgres://#{ci["username"]}:#{ci["password"]}@#{ci["hostname"] || "localhost"}/#{ci["database"]}") # Uses the postgres adapter
